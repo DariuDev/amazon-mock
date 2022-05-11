8d683ed9eee518d90ad53410ed7a37bc47b8b257
dariush <dariushtaghavi9797@gmail.com>
2022-05-11 19:31:22 +0430

    fix search

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,23 +1,20 @@
 import {Searchbar} from 'react-native-paper';
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import {ResultProvider} from '../../providers/ResultProvider';
 
 export const SearchBar = ({onNavigate}) => {
-  const {result, search, query} = useContext(ResultProvider);
-
-  useEffect(() => {
-    console.log(result, 'result')
-  }, []);
+  const {search, query} = useContext(ResultProvider);
 
   return (
     <Searchbar
       placeholder="Search"
       onIconPress={() => onNavigate('result')}
       onSubmitEditing={() => {
-        search(query)}
+       search(query) 
+        }
     }
       onChangeText={text => {
-        search(text),console.log(query, 'tect')
+        search(text)
        }}
     />
   );
